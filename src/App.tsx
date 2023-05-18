d2f2df7dc74a83433abc0766492d62ee44d13f86
hajni <richter.hajnalka29@gmail.com>
2023-05-18 12:42:57 +0100

    contact
    label, inputs, text area, button, background

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Profile from './Profile';
 
 import './App.css';
+import Contact from './Contact';
 
 function App() {
   return (
     <div className="app">
       <Profile></Profile>
+      <Contact></Contact>
     </div>
   );
 }
--- src/Contact/index.tsx
+++ src/Contact/index.tsx
@@ -2,12 +2,39 @@
 
 import "./contact.css";
 
-function Contact () {
+const Contact = () => {
   return (
     <div className="contact">
-    
-    </div>
-  );
-}
+        <div className="contact-head">
+          <div>Contact Me.</div>
+          <div className="lnr lnr-envelope"></div>
+        </div>
+          <form className="contact-concept">
+            <div className="contact-text">
+               <h2>LET'S TALK</h2>
+            </div>
+            <div className="contact-input">
+              <div className="contact-input-name">
+                  <label>Name</label>
+                  <input type="text"/>
+              </div>
+              <div className="contact-input-email"> 
+                  <label>E-mail</label>   
+                  <input type="email"/>
+              </div>
+            </div>
+            <div className="contact-input-textarea">
+                <label>How can I help you?</label>
+                <textarea></textarea>
+            </div>
+            <div className="contact-button">
+                <button>SEND MESSAGE</button>
+            </div>
+          </form>
+        <div className="contact-details">
 
+        </div>
+    </div> 
+  )
+}
 export default Contact;
